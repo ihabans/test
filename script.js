3457ca9ade90db0eebece12ebe27ae661485700a
mmhab <i.anseur@outlook.fr>
2025-08-13 18:36:27 +0200

    site

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Burger menu responsive
+const burger = document.getElementById("burger");
+const mobileNav = document.getElementById("mobileNav");
+
+burger.addEventListener("click", () => {
+  mobileNav.classList.toggle("show");
+});
+
+
+// script.js
+
+// Burger menu responsive
+
+
